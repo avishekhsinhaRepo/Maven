90b2089819021a08660064301eb0732473756dfa
Avishekh Sinha <avishekh_sinha@optum.com>
2021-08-28 20:28:03 +0530

    Maven profiles

---
+++ profilesdemo/src/main/java/com/profiles/demo/profiles/App.java
@@ -0,0 +1,13 @@
+package com.profiles.demo.profiles;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
